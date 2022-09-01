3201419d6074197f25c34bc37dcb1df2f687e495
mugu1997 <112087681+mugu1997@users.noreply.github.com>
2022-09-01 17:43:21 +0530

    done

--- resources/views/client/add_new_client.blade.php
+++ resources/views/client/add_new_client.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="add_newclient.css">
+    <link rel="stylesheet" href="assets\css\add_New.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
 
@@ -144,4 +144,3 @@
 </body>
 
 </html>
-@endsection
