c4f58cae5615e90b4b4669791016eff27951c61b
mugu1997 <112087681+mugu1997@users.noreply.github.com>
2022-09-15 11:35:25 +0530

    done

--- resources/views/client-management/client-index.blade.php
+++ resources/views/client-management/client-index.blade.php
@@ -18,19 +18,18 @@
 <div class="container-fluid">
   
   <div class="row sm">
-    <div class="col-sm-3 " > <a href=""><img src="" alt=""> <p id="m_f"> Client Handling <br>Reception</p>  </a></div>
-    <div class="col-sm-3" > <a href=""><img src="" alt=""> <p id="f_s_r">Matter</p></a></div>
-    <div class="col-sm-3" > <a href=""><img src="" alt=""> <p id="f_a">Assign Lawyer</p>      </a></div>
+    <div class="col-sm-6 " > <a><img class="logo_size" src="image\logo\icon.png" alt=""><button type="button" class="btn btn-primary btn_align_btn" > Client Handling</button></a></div>
+    <div class="col-sm-6 " > <a><img class="logo_size" src="image\logo\icon.png" alt=""><button type="button" class="btn btn-primary btn_align_btn" > Matter</button></a></div>
+    <div class="col-sm-6 " > <a><img class="logo_size" src="image\logo\icon-1.png" alt=""><button type="button" class="btn btn-primary btn_align_btn" >Assign Lawyer</button></a></div>
 
-    <div class="col-sm-3" > <a href=""><img src="" alt=""> <p id="f_r">Client Follow Up</p></a></div>
-    <div class="col-sm-3" > <a href=""><img src="" alt=""> <p id="f_n"> Complaint Registration</p>  </a></div>
-    <div class="col-sm-3" > <a href=""><img src="" alt=""> <p id="f_i"> Client Comunication</p></a> </div>
+    <div class="col-sm-6 " > <a><img class="logo_size" src="image\logo\icon-2.png" alt=""><button type="button" class="btn btn-primary btn_align_btn" > Client Follow Up</button></a></div>
+    <div class="col-sm-6 " > <a><img class="logo_size" src="image\logo\icon-3.png" alt=""><button type="button" class="btn btn-primary btn_align_btn" > Complaint Registration</button></a></div>
+    <div class="col-sm-6 " > <a><img class="logo_size" src="image\logo\icon-4.png" alt=""><button type="button" class="btn btn-primary btn_align_btn" > Client Comunication</button></a></div>
 
-    <div class="col-sm-3" > <a href=""><img src="" alt=""> <p id="o_i_l"> Client Service Reception</p></a></div>
-    <div class="col-sm-3" > <a href=""><img src="" alt=""> <p id="d_t"> Client Document</p></a> </div>
-    <div class="col-sm-3" > <a href="{{url('client_list')}}"><img src="" alt=""> <p id="m_d">Client List</p></a></div>
-
-    <div class="col-sm-3" > <a href=""><img src="" alt=""> <p id="appoint"> Client Pickup Reception</p></a>  </div>
+    <div class="col-sm-6 " > <a><img class="logo_size" src="image\logo\icon-5.png" alt=""><button type="button" class="btn btn-primary btn_align_btn" > Client Service Reception</button></a></div>
+    <div class="col-sm-6 " > <a><img class="logo_size" src="image\logo\icon-6.png" alt=""><button type="button" class="btn btn-primary btn_align_btn" > Client Document</button></a></div>
+    <div class="col-sm-6 " > <a href="{{url('client_list')}}"><img class="logo_size" src="image\logo\icon-7.png" alt=""><button type="button" class="btn btn-primary btn_align_btn" > Client List</button></a></div>
+    <div class="col-sm-6" > <a><img class="logo_size" src="image\logo\icon-8.png" alt=""><button type="button" class="btn btn-primary btn_align_btn" > Client Pickup Reception</button></a></div>
    
   </div>
 </div>
