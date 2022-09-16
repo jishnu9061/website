6a243207d96981562804a29591919e39e20addb9
Reshma171119 <reshmaglobaleyet@gmail.com>
2022-09-16 16:32:11 +0530

    commit

--- resources/views/system-settings/company_details.blade.php
+++ resources/views/system-settings/company_details.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.hmsmain')
 @section('content')
 <html>
-    <head>
-     
-      <meta charset="utf-8">
+  <head>
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">-->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -12,126 +11,68 @@
       <title>Company Details</title>
       <!-- <link rel="stylesheet" href="assets/css/cstyle.css">  -->
       <style>
-        body
-        {
-          background-color: white;
-          /* font-family:"Nunito"; */
-        }
-        label
+      h2
       {
-       
-        font-size:medium;
-        width:200px;
-        display:inline-block;
-        color: rgb(13, 1, 56); 
-        margin-left:50px;
-        font-family:"Nunito";
+        font-size:large;
+        font-weight:bold;
+        color:rgb(13, 1, 56);
+        background-color:rgb(177, 127, 62);
+        padding:15px;
+      }
+      table {
+            /* font-family: arial, sans-serif; */
+            border-collapse: collapse;
+            width: 95%;
+            margin-left:25px;
+     
+            
         }
-        input
-        {
-          width: 450px;
-          height:30px;
-          color:black;
-
-        }
-        a.button1{
-          margin-left:50px;
-  display : inline-block;
-  padding:0.35em 1.2em;
-  background-color:rgb(13, 1, 56); 
-  margin:0 0.3em 0.3em 0;
-  border-radius:0.12em;
-  box-sizing: border-box;
-  text-decoration:none;
-  font-weight:300;
-  color:rgb(240, 205, 10);
-  text-align:center;
-  transition: all 0.2s;
- 
-  }
- 
-
-  a.button2{
-    margin-left:50px;
-    display : inline-block;
-    padding:0.35em 1.2em;
-    background-color:#f0e7ce; 
-    margin:0 0.3em 0.3em 0;
-    border-radius:0.12em;
-    border-color: rgb(13, 1, 56); 
-    box-sizing: border-box;
-    text-decoration:none;
-    font-weight:300;
-    color:rgb(13, 1, 56);
-    text-align:center;
-    transition: all 0.2s;
-    outline: auto;
-  }
-  /* form{
-    background-color: #f0e7ce;
-    padding-left:1px;
-    margin-bottom:30px;
-
-  } */
-        
-</style>
+        td, th {
+            border: 1px solid rgb(240, 205, 10);
+            text-align: left;
+            padding: 8px;
+         }
+        th{
+             background-color:rgb(177, 127, 62);
+             color:rgb(13, 1, 56);
+         }
+         td{
+            color:rgb(13, 1, 56);
+         }
+      </style>
 </head>
 <body>
-  <form ><br><br> 
-  
-  	<!-- <div class="container">
-    <form class="form-horizontal" action="/action_page.php"> -->
-
-    <a href="#" class="btn btn-primary" style="margin-left:50px;background-color:rgb(13, 1, 56);color: rgb(238, 196, 105); font-weight: bold;width: 90%;">Add Company</a><br><br>
- 
-    <a href="#"  class="btn btn-seconday" style="background-color:rgb(13, 1, 56);color: rgb(238, 196, 105);margin-bottom:25px;float:right;margin-right:100px;">ADD LOGO</a><br><br><br>
-  
+<h2>Company Details</h2>
+       
+  <a href="{{url('add_company_details')}}"  class="btn btn-primary"style="color:rgb(238, 196, 105);background-color:rgb(29, 2, 129);" >Add Company</a><br><br>
+  <table>
    
- 
+  <tr>
+    <th>Sl No</th>
+    <th>Company Name</th>
+    <th>Company Address</th>
+    <th>Town/City</th>
+    <th>Logo</th>
+    <th>Edit</th>
+    <th></th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>CRA</td>
+    <td>Stima Investment Plaza,3rd floor,Mushembi Rd,Parklands</td>
+    <td>Nairobi,Keniya</td>
+    <td style="color:green;font-weight:bold;"><img src=”https://www.pexels.com/photo/high-rise-buildings-443383/” ></td>
+    <td><a href="{{url('edit_company_details')}}"style="color:blue;">Edit</a> 
+</td>
+    <!-- <td><input type="checkbox"></td> -->
+    </tr>
+
   
-
- <div class = "company">
-   <label> Company Name </label>
-  <input type="text" id="name" name="name" value=""size="50"> <br><br>
-  <label>Company Address</label>
-  <input type="text" id="address" name="address" value=""size="50">
-  <br><br>
-  <label>Town/City</label>
-  <input type="text" id="city" name="city" value=""size="50"><br><br>
-  <label>Company Website</label>
-  <input type="text" id="website" name="website" value=""size="50"><br><br>
-  <label>Company Email</label>
-  <input type="text" id="email" name="email" value=""size="50"><br><br>
-  <label>Company Type</label>
   
- <select style="width:450px;height: 30px;color:black"type="text" value="" name="type">
-    <option>   select        </option>
-    <option>Partnership</option>
-    <option>Sole partership</option>
-    <option>LLP</option>
-    <option>LTD</option>
-    <option>LLC</option>
-    <option>Others</option>
-</select><br><br>
-  <label>PIN No</label>
-  <input type="number" id="pin" name="pin" value=""size="50"><br><br>
-  <label>VAT No</label>
-  <input type="number" id="vatno" name="vatno" value=""size="50"><br><br>
-  <label>NHIF Code</label>
-  <input type="text" id="ncode" name="ncode" value=""size="50"><br><br>
-  <label>NSSF Number</label>
-  <input type="number" id="nnum" name="nnum" value=""size="50"><br><br> 
+</table><br><br>
 
-  <input  type="submit"value="Save" name="Submit"style="background-color:rgb(13, 1, 56);color: rgb(238, 196, 105); ">
-  
-  <input type="button"value="Cancel" name="cancel"style="background-color:rgb(13, 1, 56);color: rgb(238, 196, 105); ">
-<!-- <a href="something" class="button1"style="margin-left:50px;">Save</a>
-<a href="something" class="button2">Cancel</a><br><br> -->
-
-    </div>
-</form>
-<!-- </div> -->
-
-   </body>
+  <!-- <a href="{{url('edit_company_details')}}"  class="btn btn-primary"style="color:rgb(238, 196, 105);background-color:rgb(29, 2, 129);" >Edit</a><br><br> -->
+   
+</body>
 </html>
 @endsection
---
+++ resources/views/system-settings/edit_company_details.blade.php
@@ -0,0 +1,140 @@
+@extends('layouts.hmsmain')
+@section('content')
+<html>
+    <head>
+     
+      <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+
+      <title>Company Details</title>
+      <!-- <link rel="stylesheet" href="assets/css/cstyle.css">  -->
+      <style>
+        body
+        {
+          background-color: #f0e7ce;
+          /* font-family:"Nunito"; */
+        }
+        label
+      {
+       
+        font-size:medium;
+        width:200px;
+        display:inline-block;
+        color: rgb(13, 1, 56); 
+        margin-left:50px;
+        font-family:"Nunito";
+        }
+        input
+        {
+          width: 450px;
+          height:30px;
+          color:black;
+
+        }
+        a.button1{
+          margin-left:50px;
+  display : inline-block;
+  padding:0.35em 1.2em;
+  background-color:rgb(13, 1, 56); 
+  margin:0 0.3em 0.3em 0;
+  border-radius:0.12em;
+  box-sizing: border-box;
+  text-decoration:none;
+  font-weight:300;
+  color:rgb(240, 205, 10);
+  text-align:center;
+  transition: all 0.2s;
+ 
+  }
+ 
+
+  a.button2{
+    margin-left:50px;
+    display : inline-block;
+    padding:0.35em 1.2em;
+    background-color:#f0e7ce; 
+    margin:0 0.3em 0.3em 0;
+    border-radius:0.12em;
+    border-color: rgb(13, 1, 56); 
+    box-sizing: border-box;
+    text-decoration:none;
+    font-weight:300;
+    color:rgb(13, 1, 56);
+    text-align:center;
+    transition: all 0.2s;
+    outline: auto;
+  }
+  h2
+    {
+    /* font-family:  Arial, Helvetica, sans-serif; */
+    font-size:large;
+    font-weight:bold;
+    color:rgb(13, 1, 56); 
+    background-color:rgb(177, 127, 62);
+    padding:15px;
+    }
+  /* form{
+    background-color: #f0e7ce;
+    padding-left:1px;
+    margin-bottom:30px;
+
+  } */
+        
+</style>
+</head>
+<body>
+<h2>Edit Company Details</h2>
+  <form ><br><br> 
+  
+  	<!-- <div class="container">
+    <form class="form-horizontal" action="/action_page.php"> -->
+<a href="#"  class="btn btn-seconday" style="background-color:rgb(13, 1, 56);color: rgb(238, 196, 105);margin-bottom:25px;float:right;margin-right:100px;">ADD LOGO</a><br><br><br>
+  
+<div class = "company">
+   <label> Company Name </label>
+  <input type="text" id="name" name="name" value=""size="50"> <br><br>
+  <label>Company Address</label>
+  <input type="text" id="address" name="address" value=""size="50">
+  <br><br>
+  <label>Town/City</label>
+  <input type="text" id="city" name="city" value=""size="50"><br><br>
+  <label>Company Website</label>
+  <input type="text" id="website" name="website" value=""size="50"><br><br>
+  <label>Company Email</label>
+  <input type="text" id="email" name="email" value=""size="50"><br><br>
+  <label>Company Type</label>
+  
+ <select style="width:450px;height: 30px;color:black"type="text" value="" name="type">
+    <option>   select        </option>
+    <option>Partnership</option>
+    <option>Sole partership</option>
+    <option>LLP</option>
+    <option>LTD</option>
+    <option>LLC</option>
+    <option>Others</option>
+</select><br><br>
+  <label>PIN No</label>
+  <input type="number" id="pin" name="pin" value=""size="50"><br><br>
+  <label>VAT No</label>
+  <input type="number" id="vatno" name="vatno" value=""size="50"><br><br>
+  <label>NHIF Code</label>
+  <input type="text" id="ncode" name="ncode" value=""size="50"><br><br>
+  <label>NSSF Number</label>
+  <input type="number" id="nnum" name="nnum" value=""size="50"><br><br> <br>
+
+  <input  type="submit"value="Save" name="Submit"style="background-color:rgb(13, 1, 56);color: rgb(238, 196, 105); ">
+  
+  <input type="button"value="Cancel" name="cancel"style="background-color:rgb(13, 1, 56);color: rgb(238, 196, 105); ">
+<!-- <a href="something" class="button1"style="margin-left:50px;">Save</a>
+<a href="something" class="button2">Cancel</a><br><br> -->
+
+    </div>
+</form>
+<!-- </div> -->
+
+   </body>
+</html>
+@endsection
