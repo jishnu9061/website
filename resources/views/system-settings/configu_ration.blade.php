fd6d9cb87a188638977db020c803269024d0e50a
Reshma171119 <reshmaglobaleyet@gmail.com>
2022-09-17 17:19:58 +0530

    commit

--- resources/views/system-settings/configu_ration.blade.php
+++ resources/views/system-settings/configu_ration.blade.php
@@ -1,50 +1,6 @@
 @extends('layouts.hmsmain')
 @section('content')
-<html>
-    <head>
-     
-      <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<style>
-      body
-        {
-          background-color: #f0e7ce;
-          margin-left:10px;
-          margin-right:10px;
-          padding-top:10px;
-        }
-        h2
-        {
-    /* font-family:  Arial, Helvetica, sans-serif; */
-    font-size:large;
-    font-weight:bold;
-    color:rgb(13, 1, 56); 
-    background-color:rgb(177, 127, 62);
-    padding:15px;
-    
-    }
-div {
-  background-color: lightgrey;
-  width: 300px;
-  border: 1px solid green;
-  padding: 50px;
-  margin: 20px;
-}
-</style>
-</head>
-<body>
 
-<h2>Demonstrating the Box Model</h2>
-
-<p>The CSS box model is essentially a box that wraps around every HTML element. It consists of: borders, padding, margins, and the actual content.</p>
-
-<div>This text is the content of the box. We have added a 50px padding, 20px margin and a 15px green border. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-
-</body>
-</html>
 
 
     @endsection
