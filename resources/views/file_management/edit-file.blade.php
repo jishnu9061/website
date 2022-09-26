578b7f335a21d8fbbd121e1c6f36d58e8be5ba1b
vinu-201298 <112084514+vinu-201298@users.noreply.github.com>
2022-09-26 15:41:11 +0530

    commit

--- resources/views/file_management/edit-file.blade.php
+++ resources/views/file_management/edit-file.blade.php
@@ -25,7 +25,7 @@ $('#price').prop('readonly', true);
 </script>
 <script src="{{ url('assets/js') }}/jquery.min.js"></script>
 <div class="col-sm">
-    <a href="{{url('file-list')}}"><button type="submit" style=" height: 3rem; margin-left: 0%; width: 17%; margin-bottom: 13%; background-color:#acacb9;" >Back</button></a>
+   
     
 
   </div>
