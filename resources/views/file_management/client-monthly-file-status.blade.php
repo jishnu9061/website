4160ff3180a049d740f3865e4436a60f37c4bee5
anandhuglobaleyet <113765851+anandhuglobaleyet@users.noreply.github.com>
2022-10-03 14:00:19 +0530

    commit

--- resources/views/file_management/client-monthly-file-status.blade.php
+++ resources/views/file_management/client-monthly-file-status.blade.php
@@ -13,8 +13,17 @@
         </div>
         @endif
         <div>
-            <h2 style="text-align:center;">Client Monthly Status Report</h2>
-            <hr class="mb-4">.
+            <!-- <h2 style="text-align:center;">Client Monthly Status Report</h2>
+            <hr class="mb-4">. -->
+            <div>
+            <button class="btn btn-primary"
+                style="width:100%;background-color:#d6ba8a;color:#1D1D50;border:1px solid gold;font-size:25px"><b><u>Client Monthly Status Report</u></b></span></button><br>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+                integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+                crossorigin="anonymous">
+
+        
+            </div>
         </div>
     </div>
     <div class="row">
@@ -54,12 +63,14 @@
                             </div>
                         </div>
                     </div>
+</div>
+<br>
 
 
 
 
 
-
+            <div class="row">
                     <div class="col-md-6">
                         <div class="mb-1">
                             <label for="username">Month</label>
@@ -92,18 +103,18 @@
 
                 <!-- </div> -->
                 <br>
-                <div>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm">
+              
+                <div class="row">
+                        <div class="col-sm">
 
-                            </div>
-                            <div class="btn-group pt-3 " role="group" aria-label="Basic example">
+                        </div>
+                        <div class="col-sm">
 
-                                <button type="submit" class="btn btn-primary submit_btn">View Report</button>
-                                <button type="submit" class="btn btn-primary submit_btn">Close</button>
-
-                            </div>
+                        </div>
+                        <div class="col-sm">
+                            <br>
+                            <button type="submit" class="btn btn-primary float:right;" Style="width:45%;">View Report</button>
+                            <button type="button" class="btn btn-primary float:left" Style="width:45%;">Close</button>
                         </div>
                     </div>
                 </div>
