5458ae0ea49ee9f442288e05493d7761d1b7929e
Reshma171119 <reshmaglobaleyet@gmail.com>
2022-10-07 14:05:15 +0530

    commit

--- resources/views/system-settings/manage_user_account.blade.php
+++ resources/views/system-settings/manage_user_account.blade.php
@@ -33,7 +33,7 @@
    <body>
  <div>
             <button class="btn btn-primary"
-                style="width:100%;background-color:#d6ba8a;color:#1D1D50;border:1px solid gold;font-size:25px"><b><u>Manage User Accounts</u></b></span></button><br>
+                style="width:100%;background-color:#d6ba8a;color:#1D1D50;border:1px solid gold;font-size:25px"><b><u>Manage User Account</u></b></span></button><br>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
                 integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
                 crossorigin="anonymous">
