68a496cf6bd61366b6279e9db1621299ce21714e
anandhuglobaleyet <113765851+anandhuglobaleyet@users.noreply.github.com>
2022-10-25 12:03:41 +0530

    commit

--- resources/views/file_management/diary-management.blade.php
+++ resources/views/file_management/diary-management.blade.php
@@ -75,16 +75,16 @@
   <!-- The Modal -->
   <div class="modal fade" id="myModal">
                             <div class="modal-dialog modal-lg">
-                                <div class="modal-content" style="background-color:#d6ba8a">
+                                <div class="modal-content">
 
                                     <!-- Modal Header -->
-                                    <div class="modal-header" style="background-color:#d6ba8a">
+                                    <div class="modal-header">
                                         <h2 class="text-center"><b>Add Event</b></h2>
 
                                     </div>
 
                                     <!-- Modal body -->
-                                    <div class="modal-body" style="background-color:white">
+                                    <div class="modal-body">
                                         <div class="container">
                                             <form method="post" action="{{ url('diary-management') }}"
                                                 enctype="multipart/form-data">
