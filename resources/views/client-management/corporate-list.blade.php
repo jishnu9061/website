a651b3faf72d6eab61c5c3ca9fdcb1b430f439a5
anandhuglobaleyet <113765851+anandhuglobaleyet@users.noreply.github.com>
2022-10-27 12:41:20 +0530

    commit

--- resources/views/client-management/corporate-list.blade.php
+++ resources/views/client-management/corporate-list.blade.php
@@ -1,14 +1,8 @@
 @extends('layouts.hmsmain')
 @section('content')
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css"
-        href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-
+   
+   
+    
 
 {{-- <div class="spacer" style="height:40px;margin-top: 30px;"> --}}
 
@@ -18,10 +12,13 @@
 <body>
    
         <div class="container">
-            <div class="btn btn-primary"
-                style="width:100%;background-color:#d6ba8a;color:#1D1D50;border:1px solid gold;font-size:25px;">
-                <b><u>Corporate Client List</u></b>
-            </div><br><br>
+            <div>
+                <!-- style="width:100%;background-color:#d6ba8a;color:#1D1D50;border:1px solid gold;font-size:25px;">
+                <b><u>Corporate Client List</u></b> -->
+                {{-- heading --}}
+    <h4 id="hdtpa"><b>Corporate Client List</b></h4>
+    <br><br>
+            </div>
             <div>
                 <!-- <div class="btn btn-primary"
                     style="width:100%;background-color:#d6ba8a;color:#1D1D50;border:1px solid gold;font-size:25px"><b><u>Corporate Client List</u></b></span></div><br>
@@ -78,16 +75,16 @@
                         <!-- The Modal -->
                         <div class="modal fade" id="myModal">
                             <div class="modal-dialog modal-lg">
-                                <div class="modal-content" style="background-color:#d6ba8a">
+                                <div class="modal-content" >
 
                                     <!-- Modal Header -->
-                                    <div class="modal-header" style="background-color:#d6ba8a">
+                                    <div class="modal-header" >
                                         <h2 class="text-center"><b>Add Corporate Client</b></h2>
 
                                     </div>
 
                                     <!-- Modal body -->
-                                    <div class="modal-body" style="background-color:white">
+                                    <div class="modal-body" >
                                         <div class="container">
                                             <form method="post" action="{{ url('add-corporate') }}"
                                                 enctype="multipart/form-data">
@@ -408,9 +405,9 @@
                                                     </div>
                                                 </div>
                                                 <br>
-                                                <div class="btn btn-primary"
-                                                    style="width:100%;background-color:#d6ba8a;color:#1D1D50;border:1px solid gold;font-size:25px;">
-                                                    <b>Contact Person</b></span></div><br>
+                                                <div>
+                                                <h4 id="hdtpa"><b>Contact Person</b></h4>
+                                                   
                                                 <br>
                                                 <div class="row">
                                                     <div class="col-md-3">
@@ -474,7 +471,7 @@
 
                                                     </div>
                                                     <br><br>
-                                                    <div class="modal-footer" style="background-color:#d6ba8a">  
+                                                    <div class="modal-footer" >  
                                                         <button type="submit" class="btn btn-primary"style="width:15%;background-color:white;color:black">save</button>
                                                         <button type="button" class="btn btn-primary"style="width:15%;background-color:white;color:black">Cancel</button>
                                                     </div>
