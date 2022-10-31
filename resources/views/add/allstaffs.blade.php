925ea912eaed7413fd30f94814b2d1c4942d9590
ajiraj1 <93243991+ajiraj1@users.noreply.github.com>
2022-10-31 14:24:46 +0530

    commit

--- resources/views/add/allstaffs.blade.php
+++ resources/views/add/allstaffs.blade.php
@@ -1,66 +1,113 @@
 
 @extends('layouts.hmsmain')
 @section('content')
-  <link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
-  <div class="spacer" style="height:40px;margin-top: 30px;">
+<style>
+  .pagination>li>span {
+    position: relative;
+    float: left;
+    padding: 6px 12px;
+    margin-left: -1px;
+    line-height: 1.42857143;
+    color: #337ab7;
+    text-decoration: none;
+    background-color: #fff;
+    border: 1px solid #ddd;
+}
+.pagination {
+margin: 0;
+}
 
-  </div>
-<table class="table table-bordered" id="allpatients" style="margin-top: 40px;width:auto;" >
+.pagination li:hover{
+cursor: pointer;
+}
+
+.header_wrap {
+padding:30px 0;
+}
+.num_rows {
+width: 20%;
+float:left;
+}
+.tb_search{
+width: 20%;
+float:right;
+}
+.pagination-container {
+width: 70%;
+float:left;
+}
+
+.rows_count {
+width: 20%;
+float:right;
+text-align:right;
+color: #999;
+}
+</style>  
+  <h4 id="hdtpa"><b>Staff Details</b></h4>
+  <div class="container">
+    <div class="header_wrap">
+      <div class="num_rows">
+        <div class="form-group"> 	<!--		Show Numbers Of Rows 		-->
+         <select class  ="form-control" aria-label="Page navigation example" name="state" id="maxRows">
+
+          <option value="5">5</option>
+          <option value="10">10</option>
+           <option value="15">15</option>
+           <option value="20">20</option>
+           <option value="50">50</option>
+           <option value="70">70</option>
+           <option value="100">100</option>
+          <option value="5000">Show ALL Rows</option>
+          </select>
+         
+        </div>
+      </div>
+      <div class="tb_search">
+<input type="text" id="search_input_all" onkeyup="FilterkeyWord_all_table()" placeholder="Search.." class="form-control">
+      </div>
+    </div>
+<div class="table-responsive">
+<table class="table table-striped table-class" id= "table-id">
   <thead>
     <tr>
-      <th scope="col">Employee Id</th>
-      <th scope="col">Name</th>
-      {{-- <th scope="col">Role</th>
-      <th scope="col">Department</th> --}}
-      <th scope="col">Email</th>
-      <th scope="col">Phone</th>
-      <th scope="col">Sex</th>
-      <!-- <th scope="col">Edit</th> -->
-      <th scope="col">Manage</th>
+        <!-- <th>Sl No</th> -->
+        <th>User Id</th>
+        <th>Name</th>
+        <th>Email</th>
+         <th>Phone</th> 
+        <th>Action</th>
+
     </tr>
-  </thead>
-  <tbody>
-
-    @foreach($allusers as $alluser)
-
-    <tr id="data">
-      <input id="t_id" type="hidden" value="{{$alluser->id}}">
-      <th scope="row">{{$alluser->uniqueid}}</th>
-      <th id="t_name" >{{$alluser->name}}</th>
-      {{-- <th id="t_role">{{$alluser->role}}</th>
-      <td id="t_department">{{$alluser->depname}}</td> --}}
-      <th id="t_email">{{$alluser->email}}</th>
-      <th id="t_phone">{{$alluser->phone}}</th>
-      <td id="t_sex">{{$alluser->sex}}</td>
-      <!-- <td><a href="#" class="edits" data-toggle="modal" id="stafedit" data-bs-toggle="modal"
-                   data-bs-target="#default"><i class="fas fa-edit"></i></a></td> -->
-
-      @if( Auth::user()->role == 'Department Admin' )
-      <td><a href="{{url('managedepstaff'.$alluser->uniqueid)}}" style="color: green"><i class="fab fa-atlassian"></i></a></td>
-      @else
-      <td><a href="{{url('managestaff'.$alluser->uniqueid)}}" style="color: green"><i class="fab fa-atlassian"></i></a></td>
-      @endif
-      <!--
-     <td><a href="{{url('viewstaff'.$alluser->id)}}">View</a></td>
-      <td><a href="{{url('giveallowance'.$alluser->uniqueid)}}">Allowance</a></td>
-      <td><a href="{{url('givereduction'.$alluser->uniqueid)}}">Reduction</a></td> -->
-     <!--  @if($alluser->status == 'disable')
-      <th ><a href="{{url('disableuser'.$alluser->id)}}">{{$alluser->status}}</a></th>
-      @else
-      <th ><a href="{{url('enableuser'.$alluser->id)}}">{{$alluser->status}}</a></th>
-      @endif -->
-<!--       <td><a href="{{url('staffsalary'.$alluser->uniqueid)}}" style="color: green">Salary</a></td>
-      <form action="{{url('dutyschedule')}}" method="post">
-      @csrf
-      <input type="hidden" name="staffname" value="{{$alluser->name}}">
-      <input type="hidden" name="staffid" value="{{$alluser->uniqueid}}">
-      <td><button type="submit" style="color: black">Schedule</button></td> -->
-      </form>
-      </tr>
-    @endforeach
-  </tbody>
+</thead>
+<tbody>
+@foreach($allusers as $alluser)
+<tr>
+  <input id="t_id" type="hidden" value="{{$alluser->id}}">
+  <td>{{$alluser->uniqueid}}</td>
+  <td>{{$alluser->name}}</td>
+  <td>{{$alluser->email}}</td>
+  <td>{{$alluser->phone}}</td>
+  <td><a href="{{url('managestaff'.$alluser->uniqueid)}}" style="color:rgb(13, 1, 56);"><i class="fa fa-edit"></i></a></td>
+  @endforeach 
+</tr>
+ <tbody>
 </table>
-<div class="modal fade text-left" id="default" tabindex="-1" role="dialog"
+</div>
+<!--		Start Pagination -->
+    <div class='pagination-container'>
+      <nav>
+        <ul class="pagination">
+         <!--	Here the JS Function Will Add the Rows -->
+        </ul>
+      </nav>
+    </div>
+    <div class="rows_count">Showing 11 to 20 of 91</div>
+
+ <!-- 		End of Container -->
+
+
+  <div class="modal fade text-left" id="default" tabindex="-1" role="dialog"
                             aria-labelledby="myModalLabel1" aria-hidden="true">
                             <div class="modal-dialog modal-dialog-centered">
                                 <div class="modal-content">
