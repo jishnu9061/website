fa148540e0e6926f5c46a3cd686a8231f6bde147
anandhuglobaleyet <113765851+anandhuglobaleyet@users.noreply.github.com>
2022-11-03 16:53:33 +0530

    commit

--- resources/views/file_management/file-list-progress-report.blade.php
+++ resources/views/file_management/file-list-progress-report.blade.php
@@ -1,14 +1,53 @@
 @extends('layouts.hmsmain')
 @section('content')
+<html>
+    <head>
+    <style>
+  .pagination>li>span {
+    position: relative;
+    float: left;
+    padding: 6px 12px;
+    margin-left: -1px;
+    line-height: 1.42857143;
+    color: #337ab7;
+    text-decoration: none;
+    background-color: #fff;
+    border: 1px solid #ddd;
+}
+.pagination {
+margin: 0;
+}
 
-<meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css"
-        href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+.pagination li:hover{
+cursor: pointer;
+}
+
+.header_wrap {
+padding:30px 0;
+}
+.num_rows {
+width: 20%;
+float:left;
+}
+.tb_search{
+width: 20%;
+float:right;
+}
+.pagination-container {
+width: 70%;
+float:left;
+}
+
+.rows_count {
+width: 20%;
+float:right;
+text-align:right;
+color: #999;
+}
+</style>  
+ 
+    </head>
+
 <body>
 
     <div class="container">
@@ -21,11 +60,34 @@
                     <label><input type="search" class="box" placeholder="search" aria-controls="mydatatable"></label>
 
         </div> -->
+        <div class="container">
+    <div class="header_wrap">
+      <div class="num_rows">
+        <div class="form-group"> 	<!--		Show Numbers Of Rows 		-->
+         <select class  ="form-control" aria-label="Page navigation example" name="state" id="maxRows">
+
+          <option value="5">5</option>
+          <option value="10">10</option>
+           <option value="15">15</option>
+           <option value="20">20</option>
+           <option value="50">50</option>
+           <option value="70">70</option>
+           <option value="100">100</option>
+          <option value="5000">Show ALL Rows</option>
+          </select>
+         
+        </div>
+      </div>
+      <div class="tb_search">
+<input type="text" id="search_input_all" onkeyup="FilterkeyWord_all_table()" placeholder="Search.." class="form-control">
+      </div>
+    </div>
 
         <div class="tab-content" id="myTabContent">
             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
             <div class="table-responsive">
-                <table class="table table-bordered" id="new-item">
+        <table class="table table-striped table-class" id= "table-id">
+	
                     <thead >
                         <tr>
                             <th class="text-center">*</th>
@@ -66,16 +128,22 @@
                     </tbody>
 
                 </table>
+</div>
+</div>
+<!--		Start Pagination -->
+<div class='pagination-container'>
+      <nav>
+        <ul class="pagination">
+         <!--	Here the JS Function Will Add the Rows -->
+        </ul>
+      </nav>
+    </div>
+    <div class="rows_count">Showing 11 to 20 of 100</div>
+
+ <!-- 		End of Container -->
+
                 <br>
-                <nav aria-label="Page navigation example">
-                                <ul class="pagination">
-                                    <li class="page-item"><a class="page-link" href="#"style="color:#1D1D50;">Previous</a></li>
-                                    <li class="page-item"><a class="page-link" href="#"style="color:#1D1D50;">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="#"style="color:#1D1D50;">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="#"style="color:#1D1D50;">3</a></li>
-                                    <li class="page-item"><a class="page-link" href="#"style="color:#1D1D50;">Next</a></li>
-                                </ul>
-                            </nav>
+               
                 <!-- The Modal -->
                 <div class="modal fade" id="editmed">
                     <div class="modal-dialog modal-xl">
