027c45dce25e629af195f23c7a59b7cf885a15e1
anandhuglobaleyet <113765851+anandhuglobaleyet@users.noreply.github.com>
2022-11-14 14:14:30 +0530

    commit

---
+++ app/Http/Controllers/store.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+// use DB;
+
+class store extends Controller
+{
+    public function index()
+    {
+        return view('Store & Inventory.Store_&_Inventory_index');
+    }
+
+}
+
