7c281e818e9a03ad5cc6c4c6ca5bfce372f26dd5
mugu1997 <112087681+mugu1997@users.noreply.github.com>
2022-12-22 12:39:56 +0530

    done

---
+++ update_tables/Mukundhan/cra_apply_tender.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2022 at 08:06 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cra_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cra_apply_tender`
+--
+
+CREATE TABLE `cra_apply_tender` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `item_name` varchar(200) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `supplier_name` varchar(200) DEFAULT NULL,
+  `mobile_no` int(11) DEFAULT NULL,
+  `address` longtext DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `document` varchar(200) DEFAULT NULL,
+  `period_of_contract` varchar(200) DEFAULT NULL,
+  `cost_of_tender` varchar(200) DEFAULT NULL,
+  `payment_type` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cra_apply_tender`
+--
+
+INSERT INTO `cra_apply_tender` (`id`, `date`, `item_name`, `quantity`, `supplier_name`, `mobile_no`, `address`, `description`, `document`, `period_of_contract`, `cost_of_tender`, `payment_type`) VALUES
+(1, '2022-12-17', 'jvjcf', 45, 'fdfdfdfdg', 2147483647, 'jhfgsjfgbsk', 'wregtewrtwrt', 'C:\\xampp\\tmp\\phpB94A.tmp', '23', '3456778', 'card');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cra_apply_tender`
+--
+ALTER TABLE `cra_apply_tender`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cra_apply_tender`
+--
+ALTER TABLE `cra_apply_tender`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ update_tables/Mukundhan/cra_tender_details.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2022 at 08:08 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cra_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cra_tender_details`
+--
+
+CREATE TABLE `cra_tender_details` (
+  `id` int(11) NOT NULL,
+  `from_date` date DEFAULT NULL,
+  `to_date` date DEFAULT NULL,
+  `item` varchar(150) DEFAULT NULL,
+  `category` varchar(150) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cra_tender_details`
+--
+
+INSERT INTO `cra_tender_details` (`id`, `from_date`, `to_date`, `item`, `category`, `quantity`, `description`) VALUES
+(1, '2022-12-24', '2022-12-11', 'fff', 'Furniture', 45, 'dfgghh'),
+(2, '2022-12-23', '2022-12-16', 'ff', 'Furniture', 45, 'sdffff');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cra_tender_details`
+--
+ALTER TABLE `cra_tender_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cra_tender_details`
+--
+ALTER TABLE `cra_tender_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
