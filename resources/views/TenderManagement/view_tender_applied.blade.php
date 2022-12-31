f6252526f2a4aa2144560f6b27930089f8ce12f4
ajiraj1 <93243991+ajiraj1@users.noreply.github.com>
2022-12-31 13:45:28 +0530

    commit

--- resources/views/TenderManagement/view_tender_applied.blade.php
+++ resources/views/TenderManagement/view_tender_applied.blade.php
@@ -3,57 +3,48 @@
 
 
 {{-- heading --}}
-<div class="container">
-    <h4 id="hdtpa"><b>View Tender Applied Details</b></h4>
+    <h4 id="hdtpa"><b>Tender Applied Details</b></h4>
     <br>
-
-    <div class="container">
         <form method="post" action="{{url('')}}">
             @csrf
             <div class="container">
-                <div class="text-center">
+                <div class="text-left">
                     <table class="table">
                         <tbody>
-                            <tr>
-                                <td colspan="2">
-                                    <h5> Tender Details Form </h5>
-                                </td>
-                            </tr>
-
-                            <tr>
+                           <tr>
                                 <td scope="row">Date</td>
-                                <td><input class="form-control" type="text" value="{{$view_tender_applied->date}}" aria-label="input example">
+                                <td>{{$view_tender_applied->date}}
                                 </td>
 
                             </tr>
                             <tr>
                                 <td scope="row">Item Name</td>
-                                <td><input class="form-control" type="text"  value="{{$view_tender_applied->item_name}}" aria-label="input example">
+                                <td>{{$view_tender_applied->item_name}}
                                 </td>
                             </tr>
                             <tr>
                                 <td scope="row">Quantity</td>
-                                <td><input class="form-control" type="text"  value="{{$view_tender_applied->quantity}}" aria-label="input example">
+                                <td>{{$view_tender_applied->quantity}}  
                                 </td>
                             </tr>
                             <tr>
                                 <td scope="row">Supplier Name</td>
-                                <td><input class="form-control" type="text"  value="{{$view_tender_applied->supplier_name}}" aria-label="input example">
+                                <td>{{$view_tender_applied->supplier_name}}
                                 </td>
                             </tr>
                             <tr>
                                 <td scope="row">Mobile No.</td>
-                                <td><input class="form-control" type="text"  value="{{$view_tender_applied->mobile_no}}" aria-label="input example">
+                                <td>{{$view_tender_applied->mobile_no}}
                                 </td>
                             </tr>
                             <tr>
                                 <td scope="row">Address</td>
-                                <td><input class="form-control" type="text"  value="{{$view_tender_applied->address}}" aria-label="input example">
+                                <td>{{$view_tender_applied->address}}
                                 </td>
                             </tr>
                             <tr>
                                 <td scope="row">Description</td>
-                                <td><textarea class="form-control" rows="2" ></textarea></td>
+                                <td></td>
                             </tr>
                             <tr>
                                 <td scope="row">Supporting Documents</td>
@@ -63,18 +54,18 @@
                             </tr>
                             <tr>
                                 <td scope="row">Period of Contract (in Days)</td>
-                                <td><input class="form-control" type="text"  value="{{$view_tender_applied->period_of_contract}}" aria-label="input example">
+                                <td>{{$view_tender_applied->period_of_contract}}
                                 </td>
                             </tr>
                             <tr>
                                 <td scope="row">Total Cost of Tender</td>
-                                <td><input class="form-control" type="text"  value="{{$view_tender_applied->cost_of_tender}}" aria-label="input example">
+                                <td>{{$view_tender_applied->cost_of_tender}}
                                 </td>
                             </tr>
 
                             <tr>
                                 <td scope="row">Payment Type</td>
-                                <td><input class="form-control" type="text"  value="{{$view_tender_applied->payment_type}}" aria-label="input example">
+                                <td>{{$view_tender_applied->payment_type}}
                                 </td>
                             </tr>
 
@@ -100,7 +91,5 @@
 
         </form>
 
-    </div>
-</div>
 
 @endsection
