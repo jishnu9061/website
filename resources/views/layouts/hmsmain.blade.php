8718c6e7749004d9911e5a61c7f1e95ce03e22c9
BeljinRajGit <113766505+BeljinRajGit@users.noreply.github.com>
2023-01-02 18:37:44 +0530

    new nav

--- resources/views/layouts/hmsmain.blade.php
+++ resources/views/layouts/hmsmain.blade.php
@@ -212,6 +212,20 @@
             opacity: 0;
             transition: opacity 1.3s;
         }
+
+        .sidebar-menu{
+    animation: fadeInAnimation ease 1s;
+            animation-iteration-count: 1;
+            animation-fill-mode: forwards;
+        }
+        @keyframes fadeInAnimation {
+            0% {
+                opacity: 0;
+            }
+            100% {
+                opacity: 1;
+            }
+}
         </style>
 
     </head>
@@ -1005,6 +1019,81 @@ animation-direction:reverse;">
 
 
             <div id="main">
+
+
+
+
+
+
+
+
+
+
+
+
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-white">
+                    
+                    <a class="navbar-brand" href="#">
+                        <img src="{{asset('/') }}assets//images/Logo cra.png" width="40" height="40" alt="">
+                      </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                  
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                          <a class="nav-link" href="{{ url('home') }}">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                          </div>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                      </ul>
+                      <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                      </form>
+                    </div>
+                  </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 <nav class=" newstyle1 navbar navbar-expand-lg  sticky-top navbar-light bg-white">
                     <!-- Nav Back Button -->
                     <a id="backbtn" href="javascript:history.back()" style=" color: #070344;
@@ -1147,6 +1236,15 @@ animation-direction:reverse;">
                             </div>
                         </div>
                 </nav>
+
+
+
+
+
+
+
+
+
 
 
                 <!-- <a href="javascript:history.back()" class="btn btn-primary">Back</a>
