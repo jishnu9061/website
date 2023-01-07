02f60107271278384db65c3ca35cf9b7a99b825c
BeljinRajGit <113766505+BeljinRajGit@users.noreply.github.com>
2023-01-07 15:14:24 +0530

    wait for next

--- resources/views/etc/help.blade.php
+++ resources/views/etc/help.blade.php
@@ -7,6 +7,50 @@
 
 
 
+<main>
+    <h6 class="faq-heading">FAQ'S</h6>
+    <section class="faq-container">
+        <div class="faq-one">
+            <!-- faq question -->
+            <h6 class="faq-page">What is an FAQ Page?</h6>
+            <!-- faq answer -->
+            <div class="faq-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit saepe sequi, illum facere
+                    necessitatibus cum aliquam id illo omnis maxime, totam soluta voluptate amet ut sit ipsum
+                    aperiam.
+                    Perspiciatis, porro!</p>
+            </div>
+        </div>
+        <hr class="hr-line">
+        <div class="faq-two">
+            <!-- faq question -->
+            <h6 class="faq-page">Why do you need an FAQ page?</h6>
+            <!-- faq answer -->
+            <div class="faq-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit saepe sequi, illum facere
+                    necessitatibus cum aliquam id illo omnis maxime, totam soluta voluptate amet ut sit ipsum
+                    aperiam.
+                    Perspiciatis, porro!</p>
+            </div>
+        </div>
+        <hr class="hr-line">
+        <div class="faq-three">
+            <!-- faq question -->
+<h6 class="faq-page">Does it improves the user experience of a website?</h6>
+            <!-- faq answer -->
+            <div class="faq-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit saepe sequi, illum facere
+                    necessitatibus cum aliquam id illo omnis maxime, totam soluta voluptate amet ut sit ipsum
+                    aperiam.
+                    Perspiciatis, porro!</p>
+            </div>
+        </div>
+    </section>
+</main>
+<script src="main.js"></script>
+
+
+
                 <form>
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Name">
--- resources/views/hr/hrindex.blade.php
+++ resources/views/hr/hrindex.blade.php
@@ -35,6 +35,7 @@
     <!-- <div class="col-sm-4 "> <a href="{{ url('over_time') }}"><img class="logo_size"
                  src="assets\image\File list progress report.png" alt=""><button type="button"
                  style="width:70%;" class="btn btn-primary btn_align_btn"> Over Time</button></a></div> -->
+
     <div class="col-sm-4 "> <a href="{{ url('timesheet') }}"><img class="logo_size"
                 src="assets\image\File list progress report.png" alt=""><button type="button" style="width:70%;"
                 class="btn btn-primary btn_align_btn"> Time sheet</button></a></div>
@@ -72,6 +73,7 @@
 
     </div>
 </div>
+
 
 
 
