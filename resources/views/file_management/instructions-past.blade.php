a7ad62e1f3387d7744ac64b474b0c5fde3436c40
vinu-201298 <112084514+vinu-201298@users.noreply.github.com>
2023-01-25 16:48:23 +0530

    commit

--- resources/views/file_management/instructions-past.blade.php
+++ resources/views/file_management/instructions-past.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.hmsmain')
 @section('content')
+<nav style="font-size:12px;">
+      <a href="{{url('home')}}" style="color: #1D1D50;">Home</a> / 
+      <a href="#" style="color: #1D1D50;">File Management-Instructions Past</a>
+</nav>
+<br><br>
 <html>
     <head>
     <style>
