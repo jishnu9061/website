83c254e15ed3ff046c73233c6e5e1fba4b85bbae
mugu1997 <112087681+mugu1997@users.noreply.github.com>
2023-01-25 10:55:51 +0530

    commit

---
+++ update_tables/Mukundhan/salary_details.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 25, 2023 at 06:18 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cra_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salary_details`
+--
+
+CREATE TABLE `salary_details` (
+  `id` int(11) NOT NULL,
+  `unique_id` int(11) DEFAULT NULL,
+  `basic_salary` int(11) DEFAULT NULL,
+  `bank_name` varchar(150) DEFAULT NULL,
+  `account_number` varchar(150) DEFAULT NULL,
+  `branch` varchar(150) DEFAULT NULL,
+  `bank_code` varchar(150) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `salary_details`
+--
+ALTER TABLE `salary_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `salary_details`
+--
+ALTER TABLE `salary_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
