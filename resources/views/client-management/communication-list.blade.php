166cba153c0d44e0843bb6a42ebc7372e14faaff
vinu-201298 <112084514+vinu-201298@users.noreply.github.com>
2023-01-25 12:55:05 +0530

    commit

--- resources/views/client-management/communication-list.blade.php
+++ resources/views/client-management/communication-list.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.hmsmain')
 @section('content')
+<nav style="font-size:12px;">
+      <a href="{{url('home')}}" style="color: #1D1D50;">Home</a> / 
+      <a href="#" style="color: #1D1D50;">Client Management-Communication List</a>
+</nav>
+<br><br>
 
 {{-- <div class="spacer" style="height:40px;margin-top: 30px;"> --}}
 <html>
