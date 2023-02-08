f73c350e5d15fc60b561f72fed17705386eefd81
BeljinRajGit <113766505+BeljinRajGit@users.noreply.github.com>
2023-02-08 17:37:10 +0530

    Update logout_offset.blade.php

--- resources/views/navbar_logout/logout_offset.blade.php
+++ resources/views/navbar_logout/logout_offset.blade.php
@@ -1,20 +1,18 @@
 <style>
-  body {
-    font-family: 'Lato', sans-serif;
-  }
+
   
   .overlay {
-    height: 100%;
+     height: 100%;
     width: 0;
     position: fixed;
     z-index: 10000;
     top: 0;
     right: 0;
     background-color: rgb(255, 255, 255);
-    display: flex;
+   
     overflow-x: hidden;
     transition: 0.2s;
-    justify-content: space-between;
+    
   }
   .back-o {
     height: 100%;
@@ -24,42 +22,27 @@
     position: fixed;
     z-index: 9999;
     margin: auto;
-    background-color: rgb(255, 0, 0);
+  
     background-color: rgba(0,0,0, 0.5);
     overflow-x: hidden;
 
   }
-  
-  .overlay-content {
-    position: relative;
-    top: 0;
-    width: 100%;
-    right: 50px;
-    margin-top: 38px;
-  }
-  
   .overlay a {
-    padding: 8px;
     text-decoration: none;
-    font-size: 30px;
-    color: #818181;
-    display: block;
-    text-align: end;
-    transition: 0.3s;
-    
+    border: 1px solid rgb(240, 228, 227); padding:10px;font-size: 18px;background-color: #1d1d50;color: #ffffff;
+ 
+   
+  } 
+
+  .overlay ul li{
+    width:50%;
   }
   
   .overlay a:hover, .overlay a:focus {
-    color: #aca0a0;
+    color: #c5bebe;
   }
   
-  .overlay .closebtn {
-    position: absolute;
-    top: 20px;
-    left: 45px;
-    font-size: 60px;
-    text-align:left;
-  }
+
   
   @media screen and (max-height: 450px) {
     .overlay a {font-size: 20px}
@@ -69,21 +52,80 @@
     right: 35px;
     }
   }
+
+  
   </style>
 
   <div class="back-o" id="backo">
   <div id="myNav" class="overlay">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    {{-- <div class="overlay-content"> --}}
-      <a href= "{{url('logt')}}" style="
-            text-align: right;
-    height: 20px;
-    width: 100%;
-    margin: 30px 5px 10px 0px;">Log Out</a>
-      {{-- <a href="#">Services</a>
-      <a href="#">Clients</a>
-      <a href="#">Contact</a> --}}
-    {{-- </div> --}}
+    
+    <ul class="p-5"  style="
+    margin:0;
+    padding:0% 2%;
+    float: left;
+    height: 100px;
+    width:100%;
+  
+    display: flex;
+    flex-direction: row;
+    align-items: center;  
+    list-style:none;
+    ">
+
+   
+    <li style=" text-align:left;" >
+    <a style="background-color: #ffffff; color: #ff0000;" href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a></li>
+ 
+      <li style=" text-align:right;"><a href= "{{url('logt')}}">Logout</a></li>
+  
+  </ul>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <img src="assets/img/cr_logo.jpeg" class="img-fluid" alt="Responsive image">
+      </div>
+      <div class="col-sm" style="tect-align:center; display: flex;
+      flex-direction: row;
+      align-items: center; " >
+        One of three columns
+      </div>
+      
+    </div>
+  </div>
+  
+<div class="p-5">
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
   </div>
 </div>
 
