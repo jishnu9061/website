9d3da3a3cee492c68f4c047e210f5e52f3f80e3a
BeljinRajGit <113766505+BeljinRajGit@users.noreply.github.com>
2023-02-09 10:41:37 +0530

    Update logout_offset.blade.php

--- resources/views/navbar_logout/logout_offset.blade.php
+++ resources/views/navbar_logout/logout_offset.blade.php
@@ -87,6 +87,8 @@
       <div class="col-sm" style="tect-align:center; display: flex;
       flex-direction: row;
       align-items: center; " >
+
+      
         One of three columns
       </div>
       
@@ -106,20 +108,17 @@
     <tbody>
       <tr>
         <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
+      
         <td>@mdo</td>
       </tr>
       <tr>
         <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
+       
         <td>@fat</td>
       </tr>
       <tr>
         <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
+       
         <td>@twitter</td>
       </tr>
     </tbody>
