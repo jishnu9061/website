b8fe2375f3d6e356cad9d00bd83f6b7d2a6e389a
Subhasree-Nidheesh <subhasreeak@gmail.com>
2023-02-21 15:57:59 +0530

    commit
    manage files, file archive

--- routes/web.php
+++ routes/web.php
@@ -503,27 +503,25 @@ use App\Http\Controllers\issued_summary;
     Route::any('update-registration', 'CrmController@updateRegistration')->name('update-registration');
     Route::any('delete-registration/{id}', 'CrmController@deleteRegistration')->name('delete-registration');
     Route::any('show-registration/{id}', 'CrmController@showRegistration')->name('show-registration');
-    
-
-
-    
-    //manage files
+    //crm controller
+//file management controller
+        //index
     Route::any('file_managementindex', "filemanagement@index")->name('file_managementindex');
+        //manage files
     Route::any('file-list', "filemanagement@views")->name('file-list');
     Route::any('/edit-file/{id}', "filemanagement@edit")->name('edit-file');
     Route::any('/update', "filemanagement@update")->name('update');
     Route::any('view-list/{id}', 'filemanagement@viewlist')->name('view-list');
     Route::any('add-task', "filemanagement@addtask")->name('add-task');
-
     Route::any('/file_destroy/{id}', "filemanagement@file_destroy")->name('file_destroy');
-
     Route::any('add-new-file', "filemanagement@addnew")->name('add-new-file');
+        //File Archive
     Route::any('file-archive', "filemanagement@filearchive")->name('file-archive');
     Route::any('add-box-no', "filemanagement@addboxno")->name('add-box-no');
     Route::any('view-box-no', "filemanagement@viewboxno")->name('view-box-no');
     Route::any('/edit-box-no/{id}', "filemanagement@editboxno")->name('edit-box-no');
     Route::any('/update-box', "filemanagement@updatebox")->name('update-box');
-    //manage files
+    
 
     //document templates
     Route::any('template-category', "filemanagement@template")->name('template-category');
