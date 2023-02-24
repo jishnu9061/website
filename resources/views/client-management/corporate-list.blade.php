b8f702e57b891f8eac35c132e80920166c8872db
Subhasree-Nidheesh <subhasreeak@gmail.com>
2023-02-24 11:40:30 +0530

    commit
    modal-alignment correction

--- resources/views/client-management/corporate-list.blade.php
+++ resources/views/client-management/corporate-list.blade.php
@@ -1259,7 +1259,7 @@
                         <div class="row">
                             <div class="" style="*/background-color: #d3d0ca;border-radius:5px;">
                                 <div class="row">
-                                    <div class="" style="width: 33%">
+                                    <div class="" style="width: 100%">
                                         <div class=""><span style="color: red">*</span>
                                             <label for="date"
                                                 style="width: 100px;margin-bottom: 0px;margin-right: 2px;font-size:10px;">Date</label>
@@ -1269,7 +1269,7 @@
                                                 required>
                                         </div>
                                     </div>
-                                    <div class="" style="width: 70%">
+                                    <div class="" style="width: 100%">
                                         <div class=""><span style="color: red">*</span>
                                             <label for="title"
                                                 style="width: 100px;margin-bottom: 0px;margin-right: 2px;font-size:10px;">Title</label>
@@ -1326,10 +1326,6 @@
                     <div class="row document_details " style="margin-bottom: 20px;">
                         {{-- Add More Document details:- javascript --}}
                     </div>
-
-
-
-
         <div class style="width: 20%">
         </div>
         <div class="col-sm">
@@ -1339,7 +1335,7 @@
         <div class="col-sm">
 
             <button type="submit" class="btn btn-primary float:right;"
-                style="margin-left: 61%;--clr: #1D1D50;width:19%;
+                style="margin-left: 81%;--clr: #1D1D50;width:19%;
                 --outline: .001px solid var(--clr);color: white;background-color: #1D1D50;border-radius: 5px;">Post
             </button>
 
@@ -1375,7 +1371,7 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="" style="width: 70%">
+                                <div class="" style="width: 100%">
                                     <div class=""><span style="color: red">*</span>
                                         <label for="message"
                                             style="width: 100px;margin-bottom: 0px;margin-right: 2px;font-size:10px;">SMS Message</label>
@@ -1396,7 +1392,7 @@
                                             required>
                                     </div>
                                 </div>
-                                <div class="" style="width: 20%">
+                                <div class="" style="width: 30%">
                                     <div class=""><span style="color: red">*</span>
                                         <label for="time"
                                             style="width: 100px;margin-bottom: 0px;margin-right: 2px;font-size:10px;">time</label>
@@ -1410,6 +1406,8 @@
                         </div>
 
                         <div class style="width: 20%">
+                        </div>
+                        <div class="col-sm">
                         </div>
                         <div class="col-sm">
                         </div>
@@ -1449,10 +1447,6 @@
                     <div class="row document_details " style="margin-bottom: 20px;">
                         {{-- Add More Document details:- javascript --}}
                     </div>
-
-
-
-
         <div class style="width: 20%">
         </div>
         <div class="col-sm">
@@ -1462,7 +1456,7 @@
         <div class="col-sm">
 
             <button type="submit" class="btn btn-primary float:right;"
-                style="margin-left: 61%;--clr: #1D1D50;width:19%;
+                style="margin-left: 81%;--clr: #1D1D50;width:19%;
                 --outline: .001px solid var(--clr);color: white;background-color: #1D1D50;border-radius: 5px;">Post
             </button>
 
